2634f673674c63717939dac0cdf8df3a3ee5ae80
GeoffAtHome <geoff@soord.org.uk>
2017-12-10 16:04:45 +0000

    Fix startInteractiveSession resolution.

--- dispatcher.js
+++ dispatcher.js
@@ -30,7 +30,7 @@ class Dispatcher {
         var squeezeserver = new SqueezeServer(config.squeezeserverURL, config.squeezeserverPort, config.squeezeServerUsername, config.squeezeServerPassword);
         squeezeserver.on('register', function() {
             console.log("SqueezeServer registered");
-            this.startInteractiveSession(callback);
+            Dispatcher.startInteractiveSession(callback);
         });
     }
 
